58758cbc44d8e79c6aebd3156dd2a886f4c67343
Ameen5719 <naughtyameen5719@gmail.com>
2017-04-12 12:33:39 -0700

    done with profile

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ var token;
 function getval(token) {
     token = $("#input").val();
     var profileinfo = "https://graph.facebook.com/me?access_token=" + token;
-    console.log(profileinfo);
+    //console.log(profileinfo);
     var feeds = "https://graph.facebook.com/me/feed?access_token=" + token;
-    console.log(feeds);
+    //console.log(feeds);
     $.ajax({
         type: "get",
         url: profileinfo,
@@ -17,9 +17,9 @@ function getval(token) {
         success: function (data) {
             var profilepic = "https://graph.facebook.com/" + data.id + "/picture?type=square";
             var name = data.first_name + " " + data.last_name;
-            console.log(profilepic);
-            $("#profilepic").attr("src", profilepic)
-            $("#name").append(name);
+            //console.log(profilepic);
+            $("#profilepic").attr("src", pr ofilepic)
+            $("#name").append(data.name);
             $("#mail").append(data.email);
             $("#bday").append(data.birthday)
             $("#school").append(data.education[0].school.name);
@@ -28,11 +28,22 @@ function getval(token) {
                 url: feeds,
                 // dataType: "jason",
                 success: function (data) {
-
+                    var i = 0;
+                    for (i = 0; i < 4; i++) {
+                        var name = "from" + i;
+                        var story = "story" + i;
+                        var time = "time" + i;
+                        var x= data.data[i].from.name;
+                        var y= data.data[i].story;
+                        $("input[id=" + name + "]").append(x);
+                        $("input[id=" + story + "]").append(y);
+                        $("input[id=" + time + "]").append();
+                    };
                 }
             });
             $("#login").fadeOut("swing");
             $("#profile").slideDown("slow");
+            $("#feeds").fadeIn("slow");
         },
         error: function () {
             $("#login").css("background-color", "antiquewhite");
