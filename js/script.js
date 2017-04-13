3a6f5bc3246a95d3b2cc80636ae6c5473964bdbb
Ameen5719 <naughtyameen5719@gmail.com>
2017-04-13 09:08:26 -0700

    working on jquery

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function getval(token) {
             var profilepic = "https://graph.facebook.com/" + data.id + "/picture?type=square";
             var name = data.first_name + " " + data.last_name;
             //console.log(profilepic);
-            $("#profilepic").attr("src", pr ofilepic)
+            $("#profilepic").attr("src", profilepic)
             $("#name").append(data.name);
             $("#mail").append(data.email);
             $("#bday").append(data.birthday)
@@ -29,16 +29,33 @@ function getval(token) {
                 // dataType: "jason",
                 success: function (data) {
                     var i = 0;
-                    for (i = 0; i < 4; i++) {
-                        var name = "from" + i;
-                        var story = "story" + i;
-                        var time = "time" + i;
-                        var x= data.data[i].from.name;
-                        var y= data.data[i].story;
-                        $("input[id=" + name + "]").append(x);
-                        $("input[id=" + story + "]").append(y);
-                        $("input[id=" + time + "]").append();
-                    };
+                    $("#from0").append(data.data[0].from.name);
+                    $("#from1").append(data.data[1].from.name);
+                    $("#from2").append(data.data[2].from.name);
+                    $("#from3").append(data.data[3].from.name);
+                    $("#pic0").attr("src","https://graph.facebook.com/"+ data.data[0].from.id + "/picture?type=square");
+                    $("#pic1").attr("src","https://graph.facebook.com/"+ data.data[1].from.id + "/picture?type=square");
+                    $("#pic2").attr("src","https://graph.facebook.com/"+ data.data[2].from.id + "/picture?type=square");
+                    $("#pic3").attr("src","https://graph.facebook.com/"+ data.data[3].from.id + "/picture?type=square");
+                    $("#story0").append(data.data[0].story);
+                    $("#story1").append(data.data[1].story);
+                    $("#story2").append(data.data[2].story);
+                    $("#story3").append(data.data[3].story);
+                    $("a#link0").attr("herf",data.data[0].link);
+                    $("a#link1").attr("herf",data.data[1].link);
+                    $("a#link2").attr("herf",data.data[2].link);
+                    $("a#link3").attr("herf",data.data[3].link);
+
+//for (i = 0; i < 4; i++) {
+                    //    var name = "from" + i;
+                    //    var story = "story" + i;
+                    //    var time = "time" + i;
+                    //    var x= data.data[i].from.name;
+                    //    var y= data.data[i].story;
+                    //    $("input[id=" + name + "]").append(x);
+                    //    $("input[id=" + story + "]").append(y);
+                    //    $("input[id=" + time + "]").append();
+                    //};
                 }
             });
             $("#login").fadeOut("swing");
